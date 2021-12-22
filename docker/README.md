4c681af8e7569a6ec7f3a23d222ac38cc2184e9a
yamamoto <yamamoto.k@marietta.co.jp>
2021-12-22 17:12:23 +0900

    [Add]docker立ち上げ

--- docker/README.md
+++ docker/README.md
@@ -2,7 +2,7 @@
 
 ### 各ディレクトリ・ファイル説明
 ※ 以下ディレクトリとファイルパスは、READMEから見たパスです。
-* ./docker_centos : apache・phpの設定ファイルの置き場所
+* ./docker_alma : apache・phpの設定ファイルの置き場所
 * ./docker_mysql : mysqlの設定ファイルの置き場所
 * ../db : データベース保存場所
 * ./docker_init.sh : ./docker_centosと./docker_mysql内の設定ファイルをコンテナ内にコピーする時に使用するシェルスクリプト、その他起動時必要な処理
--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
-docker cp ./docker_centos/httpd/app.conf ryutoen-web-container:/etc/httpd/conf.d/app.conf
-docker cp ./docker_centos/php/00-app.ini ryutoen-web-container:/etc/php.d/
-docker exec -it ryutoen-db-container /usr/bin/mysql_tzinfo_to_sql /usr/share/zoneinfo > ~/timezone.sql
-docker exec -it ryutoen-db-container mysql -u root -p -Dmysql < ~/timezone.sql
-docker cp ./docker_mysql/my.cnf ryutoen-db-container:/etc/mysql/conf.d/
-docker cp ./docker_centos/ssl/localhost.key ryutoen-web-container:/etc/pki/tls/private/
-docker cp ./docker_centos/ssl/localhost.crt ryutoen-web-container:/etc/pki/tls/certs/
-docker exec -it ryutoen-web-container systemctl restart httpd
-docker exec -it ryutoen-db-container service mysql restart
-docker exec -it ryutoen-web-container php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-docker exec -it ryutoen-web-container php composer-setup.php --version=1.10.10
-docker exec -it ryutoen-web-container php -r "unlink('composer-setup.php');"
-docker exec -it ryutoen-web-container mv -f composer.phar /usr/local/bin/composer
+docker cp ./docker_alma/httpd/app.conf tcc-dm-web-container:/etc/httpd/conf.d/app.conf
+docker cp ./docker_alma/php/00-app.ini tcc-dm-web-container:/etc/php.d/
+docker exec -it tcc-dm-db-container /usr/bin/mysql_tzinfo_to_sql /usr/share/zoneinfo > ~/timezone.sql
+docker exec -it tcc-dm-db-container mysql -u root -p -Dmysql < ~/timezone.sql
+docker cp ./docker_mysql/my.cnf tcc-dm-db-container:/etc/mysql/conf.d/
+docker cp ./docker_alma/ssl/localhost.key tcc-dm-web-container:/etc/pki/tls/private/
+docker cp ./docker_alma/ssl/localhost.crt tcc-dm-web-container:/etc/pki/tls/certs/
+docker exec -it tcc-dm-web-container systemctl restart httpd
+docker exec -it tcc-dm-db-container service mysql restart
+docker exec -it tcc-dm-web-container php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+docker exec -it tcc-dm-web-container php composer-setup.php --version=1.10.10
+docker exec -it tcc-dm-web-container php -r "unlink('composer-setup.php');"
+docker exec -it tcc-dm-web-container mv -f composer.phar /usr/local/bin/composer
